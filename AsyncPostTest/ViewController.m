c09a40550c1f9db071f4e2a3e2e982f60d1a1133
JD Vega <jdvega@JDs-Mac-Pro.local>
2012-11-28 15:33:19 -0600

    This is the working code for posting to data to the PHP service and receiving a response.

--- AsyncPostTest/ViewController.m
+++ AsyncPostTest/ViewController.m
@@ -14,20 +14,67 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad
-{
+-(IBAction)postData:(id)sender {
+    
+    NSURL *myURL = [NSURL URLWithString:@"http://thesocialbull.com/dev/pad_dev/services/saveNewClientData.php"];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:myURL
+                                                           cachePolicy:NSURLRequestReloadIgnoringLocalCacheData
+                                                       timeoutInterval:60];
+    [request setHTTPMethod:@"POST"];
+    NSMutableString *body = [[NSMutableString alloc] initWithFormat:@"FIRST_NAME=%@&LAST_NAME=%@&ADDRESS=%@&CITY=%@&STATE=%@&ZIP=%@&PHONE=%@&EMAIL=%@&GENDER=%@&AGE=%@&NOTES=%@",
+                      @"Mickey", 
+                      @"Mouse", 
+                      @"123 Disney Dr.", 
+                      @"Los Angeles", 
+                      @"CA", 
+                      @"12345", 
+                      @"215-232-2334", 
+                      @"mickey@disney.com", 
+                      @"male",
+                      @"99",
+                      @"No Notes"];
+    
+    [request setHTTPBody: [body dataUsingEncoding:NSUTF8StringEncoding]] ; 
+     //NSUTF8StringEncoding
+     //NSISOLatin1StringEncoding
+     //NSASCIIStringEncoding
+    
+    [[NSURLConnection alloc] initWithRequest:request delegate:self];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
+    responseData = [[NSMutableData alloc] init];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
+    [responseData appendData:data];
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
+    [responseData release];
+    [connection release];
+    NSLog(@"Unable to fetch data");
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
+    NSLog(@"Succeeded! Received %d bytes of data",[responseData
+                                                   length]);
+    NSString *s = [[[NSString alloc] initWithData:responseData encoding: NSASCIIStringEncoding] retain] ;
+    NSLog( @"%@",s );
+    
+}
+
+- (void)viewDidLoad{
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)viewDidUnload
-{
+- (void)viewDidUnload{
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
 
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation{
     return YES;
 }
 
